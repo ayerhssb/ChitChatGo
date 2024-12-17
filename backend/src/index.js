076db9c9e4718e41452fa4a7c7cdb0a591f1780a
ayerhssb <shreyabhartissb1@gmail.com>
2024-12-18 05:23:43 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoutes);
+app.listen(PORT,()=>{
+    console.log(`server running on port: ${PORT}`);
+    connectDB();
+})
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,53 @@
+import {create} from "zustand";
+// import { checkAuth } from "../../../backend/src/controllers/auth.controller";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async()=>{
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser:res.data})
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({authUser: null});
+        } finally{
+            set({isCheckingAuth:false});
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/auth/signup", data);
+          set({ authUser: res.data });
+          toast.success("Account created successfully");
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isSigningUp: false });
+        }
+      },
+
+      login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/auth/login", data);
+          set({ authUser: res.data });
+          toast.success("Logged in successfully");
+    
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isLoggingIn: false });
+        }
+      },
+}));
+
